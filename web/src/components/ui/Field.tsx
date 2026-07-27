78c2c0245d9d7c585cbdfee7dc9f749d98dc388e
hamza.sdsol <hamza.sdsol@gmail.com>
2026-07-27 12:32:26 +0500

    fixes

--- web/src/components/ui/Field.tsx
+++ web/src/components/ui/Field.tsx
@@ -37,7 +37,7 @@ export function Select(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
   return (
     <select
       {...props}
-      className={`${fieldBase} appearance-none bg-[length:16px] bg-[right_14px_center] bg-no-repeat pr-9 ${props.className ?? ""}`}
+      className={`${fieldBase} appearance-none bg-size-4 bg-position-[right_14px_center] bg-no-repeat pr-9 ${props.className ?? ""}`}
       style={{
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24' fill='none' stroke='%2364748b' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='m6 9 6 6 6-6'/%3E%3C/svg%3E")`,
         ...(props.style ?? {}),
